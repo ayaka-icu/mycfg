bed7eb409483479e75b8c0cf3fba50af0d7b7587
Ayaka <2282514478@qq.com>
2023-03-23 00:45:45 +0800

    //

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -4,5 +4,6 @@ source $ZSH/config/git.zsh
 source $ZSH/config/zsh.zsh
 source $ZSH/config/fzf.zsh
 
+
 # 隐私配置 可将非通用的配置写入 config/private.zsh 注意此文件不会随git管理
 [ -f $ZSH/config/private.zsh ] && source $ZSH/config/private.zsh
